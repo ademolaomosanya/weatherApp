b5f743e705011c2b8b17389a3dfc6ba80ed71305
ademola <omosanyaademola@gmail.com>
2022-09-30 19:10:59 +0100

    completed weather app

---
+++ weather/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions  = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '8a3a0e3cc1msh081bcedf62fb720p1ec866jsn7dccc1d76c36',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
+    export const GEO_API_URL ='https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+    export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+    export const WEATHER_API_KEY = "d01a74b0f2c1ef0a8c9a7295104b82ee";
